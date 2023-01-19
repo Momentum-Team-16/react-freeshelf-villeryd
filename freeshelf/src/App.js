2e354a4b3b7e066e60d8cc2cfdc38ee3fbee13b3
Daniel <danielvillery@gmail.com>
2023-01-19 16:12:14 -0500

    created freeshelf example

--- freeshelf/src/App.js
+++ freeshelf/src/App.js
@@ -1,18 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  const devs = ['Daniel','Cameron','Lucian','Marcus']
+
+function App(props) { 
+  const [devs, setDevs] = useState([
+  {name: 'Daniel', expertise:'JS, beats'},
+  {name:'Cameron', expertise:'JS,  Soccer'},
+  {name:'Lucian', expertise:'JS, Corny Memes'},
+  {name:'Marcus', expertise:'JS, Comic Books'},
+])
   return (
     <div className="App">
       <h1>React Devs for Hire</h1>
-      <ul>
-        {
-          devs.map()
-        }
-      </ul>
+      <div className='dev-list' id ='dev-list'>
+        {devs.map(dev => <Developer name={dev.name} expertise={dev.expertise}/>)}
+      </div>
     </div>
   );
 }
 
+function Developer({name, expertise}){
+
+  const[expanded, setExpanded] = useState(false)
+  const handleClick = () => setExpanded(!expanded)
+  return(
+    <div onClick={handleClick} style={{border: 'solid 1px silver', margin: '5px', padding:'10px'}}> 
+            <p>{name}</p>
+            { expanded && <p>{expertise}</p>}
+        </div>
+  )
+
+}
 export default App;
--- freeshelf/src/index.js
+++ freeshelf/src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const devs = ['Daniel','Cameron','Lucian','Marcus']
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App devs={devs}/>
   </React.StrictMode>
 );
 
